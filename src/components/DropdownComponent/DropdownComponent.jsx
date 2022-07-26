af976324d42688ba9eaa9753b968e96224bf7e68
Morgan <morgand@tutamail.com>
2022-07-26 18:16:48 +0200

    AboutUsPage - js dropdown + css

--- src/components/DropdownComponent/DropdownComponent.jsx
+++ src/components/DropdownComponent/DropdownComponent.jsx
@@ -1,12 +1,31 @@
+import './DropdownComponent.css'
+import chevronUp from "../../assets/icon/chevron_haut.svg"
+
+function toggleDropdown(para) {
+    // sélection contenu et icon du dropdown 
+    const dropContent = para.parentElement.nextElementSibling;
+    const dropIcon = para.previousElementSibling;
+
+    if (dropContent.classList.contains("hide")) {
+        dropContent.classList.remove("hide");
+        dropContent.classList.add("appear");
+        dropIcon.classList.add("rotate");
+    } else {
+        dropContent.classList.remove("appear");
+        dropContent.classList.add("hide");
+        dropIcon.classList.remove("rotate");
+    }
+}
 
 function DropdownComponent({ heading, content }) {
 
     return (
-        <section>
-            <div className="dropdownHeading">
+        <section className="dropdownSection">
+            <div onClick={(e) => toggleDropdown(e.target)} className="dropdownHeading">
+                <img className="chevronUp" src={chevronUp} alt="" />
                 <h2>{heading}</h2>
             </div>
-            <div className="dropdownContent">
+            <div className="dropdownContent hide">
                 {
                     Array.isArray(content) ?
                         <ul>
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -6,36 +6,38 @@ import DropDownComponent from "../../components/DropdownComponent/DropdownCompon
 const aboutUsData = [
     {
         heading: "Fiabilité",
-        description: "Lorem ipsum",
+        description: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
     },
     {
         heading: "Respect",
-        description: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme",
+        description: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
     },
     {
         heading: "Service",
-        description: "Lorem ipsum"
+        description: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
     },
     {
-        heading: "Responsabilité",
-        description: "Lorem ipsum"
+        heading: "Sécurité",
+        description: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
     }
 ]
 
 
 function AboutUsPage() {
-    
+
     return (
-        <main>
-            <Banner title="" image={imgBanAbout} height="195px" />
-            {aboutUsData.map((el, index) => {
-                return (<DropDownComponent heading={el.heading} 
-                    content={el.description} 
-                    key={`${el.heading}-${index}`}
+        <div>
+            <Banner title="" image={imgBanAbout} height="223px" />
+            <main className="aboutUsDropdowns">
+                {aboutUsData.map((el, index) => {
+                    return (<DropDownComponent heading={el.heading}
+                        content={el.description}
+                        key={`${el.heading}-${index}`}
                     />)
-            }
-            )}
-        </main>
+                }
+                )}
+            </main>
+        </div>
     )
 }
 
