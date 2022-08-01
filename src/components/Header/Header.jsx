7c7ded73eddff6241cc4b9726b586bfcd0f0d88c
Morgan <morgand@tutamail.com>
2022-08-01 13:34:22 +0200

    homePage css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import './Header.css'
-import logoKasaMobile from '../../assets/logoKasaMobile.svg'
-
+import './Header.css';
+import logoKasaMobile from '../../assets/logoKasaMobile.svg';
 
 function Header() {
-
     return (
         <header>
-            <div className='logoKasa'>
-                <img src={logoKasaMobile} alt='logo Kasa' />
+            <div className="logoKasa">
+                <img src={logoKasaMobile} alt="logo Kasa" />
             </div>
 
             <nav>
-                <Link className="linkAccueil" to="/">ACCUEIL</Link>
+                <Link className="linkAccueil" to="/">
+                    ACCUEIL
+                </Link>
                 <Link to="/aboutus">A PROPOS </Link>
             </nav>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
