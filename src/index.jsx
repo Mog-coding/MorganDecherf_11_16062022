fc3ff08de62b11df153538448d34f6468539ee93
Morgan <morgand@tutamail.com>
2022-08-14 15:53:18 +0200

    add custom hook useFetch

--- src/index.jsx
+++ src/index.jsx
@@ -9,27 +9,28 @@ import HousingPage from './pages/HousingPage/HousingPage';
 import AboutUsPage from './pages/AboutUsPage/AboutUsPage';
 import Error404 from './pages/Error404/Error404';
 import Footer from './components/Footer/Footer';
+import Truc from './pages/truc/Truc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Header />
-            <Switch>
-                <Route exact path="/">
-                    <HomePage />
-                </Route>
-                <Route path="/housing/:id">
-                    <HousingPage />
-                </Route>
-                <Route path="/aboutus">
-                    <AboutUsPage />
-                </Route>
-                <Route path="/404">
-                    <Error404 />
-                </Route>
-            </Switch>
-            <Footer />
-        </Router>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <Router>
+        <Header />
+        <Switch>
+            <Route exact path="/">
+                <HomePage />
+            </Route>
+            <Route path="/housing/:id">
+                <HousingPage />
+            </Route>
+            <Route path="/aboutus">
+                <AboutUsPage />
+            </Route>
+            <Route path="/404">
+                <Error404 />
+            </Route>
+        </Switch>
+        <Footer />
+    </Router>
+    // </React.StrictMode>
 );
